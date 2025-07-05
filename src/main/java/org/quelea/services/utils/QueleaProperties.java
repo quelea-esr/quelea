9b1717cf1ea6b8604f6d1a839388893c03a3d699
Ebbez <7920708+ebbez@users.noreply.github.com>
2025-07-05 18:31:44 +0200

    ci: remove snap & cp-installer, change minor version nr to month

--- src/main/java/org/quelea/services/utils/QueleaProperties.java
+++ src/main/java/org/quelea/services/utils/QueleaProperties.java
@@ -176,7 +176,7 @@ import static org.quelea.services.utils.QueleaPropertyKeys.wikiPageKey;
  */
 public final class QueleaProperties extends SortedProperties {
 
-    public static final Version VERSION = new Version("2025.0", VersionType.CI);
+    public static final Version VERSION = new Version("2025.07", VersionType.CI);
     private static QueleaProperties INSTANCE;
     private String userHome;
 
